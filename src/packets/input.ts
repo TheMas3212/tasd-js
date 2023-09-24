3f7c96f26d2613aa522ff3a0c7dcf877677c1e97
TheMas3212 <mas3212@gmail.com>
2023-09-24 20:53:18 +1000

    Cleanup InputChunk

--- src/packets/input.ts
+++ src/packets/input.ts
@@ -1,4 +1,5 @@
 import { PACKET_TYPES } from "../constants";
+import { readUint32, readUint64, readUint8, writeUint32, writeUint64, writeUint8 } from "../utils";
 import { TASDPacket, buildBuffer } from "./utils";
 
 export class InputChunkPacket implements TASDPacket {
@@ -10,11 +11,11 @@ export class InputChunkPacket implements TASDPacket {
     return this.inputs.length + 1;
   }
   static fromBuffer(buffer: Uint8Array) {
-    return new InputChunkPacket(buffer[0], buffer.subarray(1));
+    return new this(readUint8(buffer, 0), buffer.subarray(1));
   }
   toBuffer(g_keylen: number): Uint8Array {
     const payload = new Uint8Array(this.inputs.length + 1);
-    payload[0] = this.port;
+    writeUint8(this.port, payload, 0);
     payload.set(this.inputs, 1);
     return buildBuffer(g_keylen, this.key, payload);
   }
